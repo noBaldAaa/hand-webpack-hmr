ef033091fb26dc38789bbd0f0fabc828248076a5
wxy <102939294+noBaldAaa@users.noreply.github.com>
2022-12-06 21:04:27 +0800

    支持reactd的hmr

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      作者：不要秃头啊
+      <input />
+    </div>
+  );
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,27 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
 
 module.exports = {
   mode: "development", //开发模式
-  entry: "./src/index.js", //入口
+  entry: "./src/index.jsx", //入口
+  // entry: "./src/index.js"
   devServer: {
     hot: true, //开启热更新
     port: 8000, //设置端口号
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./index.html", //将打包后的代码插入到html模版中
     }),
+    new ReactRefreshWebpackPlugin(),
   ],
 };
